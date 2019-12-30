113773a7e1858265e3298cdcb2512fc810572ce8
Pol Thongtaem <pthongtaem@hotmail.com>
2019-12-30 10:11:35 +0700

    add react router

--- template/src/App.js
+++ template/src/App.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import WebRoute from './config/WebRoute';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://origin.co.th"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Origin Property
-        </a>
-      </header>
-    </div>
-  );
+  return <WebRoute />;
 }
 
 export default App;
---
+++ template/src/pages/Home/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://origin.co.th"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Origin Property
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
